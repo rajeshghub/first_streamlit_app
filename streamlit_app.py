deb438a5604e249a62fabd73822536dd71fc5a4d
rajeshghub <140414352+rajeshghub@users.noreply.github.com>
2023-07-24 19:42:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+steramlit.header('Breakfast Menu')
+steramlit.text('Omega 3 & Blueberry Oatmeal')
+steramlit.text('Kale, Spinach & Rocket Smoothie')
+steramlit.text('Hard-Boiled Free-Range Egg')
